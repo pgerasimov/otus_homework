59502f470d7244733fdbb6939149016269da7b97
p.gerasimov <gerasimov86@gmail.com>
2023-11-15 18:50:16 +0300

    project structure

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,2 +1,40 @@
+from flask import Flask, render_template
+from flask_migrate import Migrate
+from flask_login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    current_user)
+
+from webapp.model import db, Users
+import logging
+
+
 def create_app():
-    pass
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    logging.basicConfig(filename='app.log',
+                        filemode='w',
+                        level=logging.ERROR,
+                        datefmt='%m/%d/%Y %I:%M:%S %p',
+                        format='%(name)s - %(levelname)s - %(message)s')
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'registration'
+
+    @app.after_request
+    def add_header(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        response.headers['Cache-Control'] = 'public, max-age=0'
+        return response
+
+    @app.route("/")
+    def index():
+        return render_template('base.html', active='index')
